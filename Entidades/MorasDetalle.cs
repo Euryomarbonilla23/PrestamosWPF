13abafb729e9b355eee69b0ad43a058a002ca936
Eury Bonilla <Eury_bonilla@ucne.edu.do>
2020-10-09 01:08:40 -0400

    Creando la entidad Moras

--- Entidades/MorasDetalle.cs
+++ Entidades/MorasDetalle.cs
@@ -6,6 +6,10 @@ namespace PrestamosWPF.Entidades
 {
     public class MorasDetalle
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public int MoraId { get; set; }
+        public int PrestamoId  { get; set; }
+        public decimal Valor { get; set; }
     }
 }
