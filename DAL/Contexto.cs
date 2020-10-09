329c544c8729e26952545633289e438fed920de2
Eury Bonilla <Eury_bonilla@ucne.edu.do>
2020-10-09 01:13:09 -0400

    haciendo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PrestamosWPF.Entidades;
+
+namespace PrimerParcial.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Moras> Moras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = DATA/Database.db");
+        }
+    }
+}
